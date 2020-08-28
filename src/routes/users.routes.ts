6e764e319cccc798235c23992aee61d71c6e1c08
AntonioNtV <antoniontv18@gmail.com>
2020-08-28 20:14:14 -0300

    Minor fix: funçao deveria ser async

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,12 +3,14 @@ import CreateUserService from '../services/CreateUserService';
 
 const userRouter = Router();
 
-userRouter.post('/', (request, response) => {
+userRouter.post('/', async (request, response) => {
     const { email, password } = request.body;
 
     const createUser = new CreateUserService();
 
-    const user = createUser.execute({ email, password });
+    const user = await createUser.execute({ email, password });
+
+    delete user.password;
 
     return response.json(user);
 });
