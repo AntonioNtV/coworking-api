8dca5fcfa9deece73f44e4380c4b728fd726994f
AntonioNtV <antoniontv18@gmail.com>
2020-08-28 21:19:26 -0300

    Movendo a responsabilidade de tratamento de erros globais para dentro de um arquivo proprio

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,16 @@
 import 'express-async-errors';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import routes from './routes';
-import AppError from './errors/AppError';
 
 import './database';
+import globalExceptionTreater from './middlewares/globalExceptionTreater';
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
 
-app.use(
-    (error: Error, request: Request, response: Response, _: NextFunction) => {
-        if (error instanceof AppError) {
-            return response.status(error.statusCode).json({
-                status: 'error',
-                message: error.message,
-            });
-        }
-
-        console.error(error);
-
-        return response.status(500).json({
-            status: 'error',
-            message: 'Internal server error',
-        });
-    },
-);
+app.use(globalExceptionTreater);
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333');
