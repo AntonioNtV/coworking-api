d81f8d42edbc862764310c3da343f2845d41cc28
AntonioNtV <antoniontv18@gmail.com>
2020-08-28 21:13:20 -0300

    Rota de atualizaçao de uma workstation

--- src/routes/workstation.routes.ts
+++ src/routes/workstation.routes.ts
@@ -1,16 +1,33 @@
 import { Router } from 'express';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateWorkstationService from '../services/CreateWorkstationService';
+import UpdateWorkstationService from '../services/UpdateWorkstationService';
 
 const workstationRouter = Router();
 
 workstationRouter.use(ensureAuthenticated);
+
 workstationRouter.post('/', async (request, response) => {
     const { name, description } = request.body;
 
     const createWorkstation = new CreateWorkstationService();
 
     const workstation = await createWorkstation.execute({ name, description });
+
+    return response.json(workstation);
+});
+
+workstationRouter.put('/:id', async (request, response) => {
+    const { name, description } = request.body;
+    const { id } = request.params;
+
+    const updateWorkstation = new UpdateWorkstationService();
+
+    const workstation = await updateWorkstation.execute({
+        id,
+        name,
+        description,
+    });
 
     return response.json(workstation);
 });
