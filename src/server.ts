569838faad9d2f1998aabb71dccefe98d4cc4f64
AntonioNtV <antoniontv18@gmail.com>
2020-09-11 20:09:32 -0300

    Adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
@@ -7,6 +8,7 @@ import globalExceptionTreater from './middlewares/globalExceptionTreater';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
